e606ce0ff5ce288556e8ca60ef56f15fead7a572
Monsterned <andrewl.12.al85@gmail.com>
2024-06-20 18:14:34 -0300

    tentando rodar no not

--- Robo_Baixa_Entregas.py
+++ Robo_Baixa_Entregas.py
@@ -329,10 +329,11 @@ driver.maximize_window()
 
 click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span/button')
 
-click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span[2]/button')
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span/button')
+
 try:
     print("Pasta Planilha Bahia...")
-    corpo_email = WebDriverWait(driver, 360).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span[2]/button')))
+    corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
     action_chains = ActionChains(driver)
     action_chains.context_click(corpo_email).perform()
 except Exception as e:
@@ -342,23 +343,23 @@ pyautogui.sleep(2)
 driver.back()
 pyautogui.sleep(2)
 
-click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div[3]/div/div[1]/span/span[2]/button')
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div[3]/div/div[1]/span/span/button')
 try:
     print("Pasta Planilha CC15...")
-    corpo_email = WebDriverWait(driver, 360).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span[2]/button')))
+    corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
     action_chains = ActionChains(driver)
     action_chains.context_click(corpo_email).perform()
 except Exception as e:
     print("Erro ao clicar no corpo do e-mail:", e)
-click_selenium(By.XPATH, '/html/body/div[3]/div/div/div/div/div/div/ul/li[4]/button/div/span')
+click_selenium(By.XPATH, '/html/body/div[4]/div/div/div/div/div/div/ul/li[4]/button/div/span')
 pyautogui.sleep(2)
 driver.back()
 pyautogui.sleep(2)
 
-click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[3]/div/div/div[3]/div/div[1]/span/span[2]/button')
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[3]/div/div/div[3]/div/div[1]/span/span/button')
 try:
     print("Pasta Planilha CC19...")
-    corpo_email = WebDriverWait(driver, 360).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span[2]/button')))
+    corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
     action_chains = ActionChains(driver)
     action_chains.context_click(corpo_email).perform()
 except Exception as e:
--- teste.py
+++ teste.py
@@ -19,52 +19,33 @@ import shutil
  
 caminho = os.getcwd() 
 
-def click_image(image_path, confidence=0.8):
-    current_dir = os.path.dirname(__file__)  # Diretório atual do script
-    caminho_imagem = caminho + r'\IMAGENS'
-    image_path = os.path.join(current_dir, caminho_imagem, image_path) 
-    while True:
-        try:
-            position = pyautogui.locateOnScreen(image_path, confidence=confidence)
-            if position:
-                center_x = position.left + position.width // 2
-                center_y = position.top + position.height // 2
-                pyautogui.click(center_x, center_y)
-                print("Imagem encontrada e clicada.")
-                break
-        except:
-            print("Imagem não encontrada na tela. Aguardando...")
-        pyautogui.sleep(1)
+def click_selenium(selector, value):
+    try:
+        print("Clicando no botão...")
+        elemento = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((selector, value)))
+        elemento.click()
+    except Exception as e:
+        print(f"Erro ao clicar: {e}")
 
-click_image('logo_rodopar_areatrabalho_resumido.png')#PC ESCRITORIO
-#click_image('logo_rodopar_areatrabalho.png')#PC CASA
-pyautogui.click()
-click_image('conectar_rodopar.png')
-#click_image('conectar_rodopar1.png')
-click_image('senha_rodopar_1.png')
-#click_image('senha_rodopar_2.png')
-pyautogui.write("17@mudar")
-click_image('ok_primeiro_login.png')
-#click_image('ok_primeiro_login2.png')
-click_image('sim_primeiro_login.png')
-#click_image('sim_primeiro_login2.png')
-click_image('segundo_login.png')    
-pyautogui.sleep(1)
-pyautogui.write("anascimento")
-pyautogui.press("tab")
-pyautogui.write("990607")
-for i in range(2): 
-    pyautogui.press("enter")
-click_image('filial_1.png')
-pyautogui.press("enter")
+driver = webdriver.Chrome()
+driver.get("https://jettatransporte-my.sharepoint.com/:f:/g/personal/jetta_bi_jettatransporte_onmicrosoft_com/EiA6eCcrmHVOi0SVjgVS4eYBTgW6NmdHNlvRSINLlAOW5g?e=qyl9wK")
+driver.maximize_window()
 
-click_image('botao_frota.png')
-pyautogui.press("alt")
-pyautogui.press("alt")
-pyautogui.press("right")
-for i in range(2): 
-    pyautogui.press("down")
-pyautogui.press("right")
-for i in range(10): 
-    pyautogui.press("down")
-pyautogui.press("enter")
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span/button')
+
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span/button')
+
+try:
+    print("Pasta Planilha Bahia...")
+    corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
+    action_chains = ActionChains(driver)
+    action_chains.context_click(corpo_email).perform()
+except Exception as e:
+    print("Erro ao clicar no corpo do e-mail:", e)
+
+click_selenium(By.XPATH, '/html/body/div[3]/div/div/div/div/div/div/ul/li[4]/button/div')
+
+# pyautogui.sleep(2)
+# driver.back()
+# pyautogui.sleep(2)
+
