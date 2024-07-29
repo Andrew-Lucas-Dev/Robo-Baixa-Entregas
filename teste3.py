d3e53d1e24a2832eca9ef7252bf9e817455d9a7f
Monsterned <andrewl.12.al85@gmail.com>
2024-07-29 09:35:36 -0300

    atualizado

--- teste3.py
+++ teste3.py
@@ -15,7 +15,8 @@ from selenium.common.exceptions import TimeoutException
 import pyautogui
 from selenium.webdriver.common.action_chains import ActionChains
 import shutil
-
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 caminho = os.getcwd() 
 caminho_sistema = caminho.replace("C", "T", 1)
@@ -92,11 +93,77 @@ processar_coluna_chegada(Planilha_CC19,'Data Chegada')
 Planilha_CC19 = Planilha_CC19.dropna(axis=1, how='all')
 #print(Planilha_CC19)
 
+Planilha_CC15 = pd.read_excel("planilhaderotascc15.xlsx")
+colunas_para_remover = ['Série', 'Cnpj cliente', 'N° Carga', 'Status da baixa','Cliente','Cidade','Ct-e/OST','Peso','Qtde','Vlr Merc.','Entrega Canhoto Físico']
+Planilha_CC15.drop(columns=colunas_para_remover, inplace=True)
+Planilha_CC15['N° NF'] = pd.to_numeric(Planilha_CC15['N° NF'], errors='coerce')
+Planilha_CC15.dropna(subset=['N° NF'], inplace=True)
+Planilha_CC15['N° NF'] = Planilha_CC15['N° NF'].astype(int)
+Planilha_CC15 = Planilha_CC15.rename(columns={'N° NF': 'NF', 'Data NF': 'DATA NOTA FISCAL', 'Data': 'Data Chegada', 'Status da entrega': 'STATUS'})
+Planilha_CC15['Data Entrega'] = Planilha_CC15['Data Chegada']
+Planilha_CC15['Fim Descarreg.'] = Planilha_CC15['Data Chegada']
+Planilha_CC15['STATUS'] = Planilha_CC15['STATUS'].fillna('EM ROTA')
+Planilha_CC15['DATA NOTA FISCAL'] = pd.to_datetime(Planilha_CC15['DATA NOTA FISCAL'])
+Planilha_CC15['DATA NOTA FISCAL'] = Planilha_CC15['DATA NOTA FISCAL'].dt.strftime('%d/%m/%Y')
+colunas_de_data = ['Data Entrega', 'Fim Descarreg.']
+for coluna in colunas_de_data:
+    processar_coluna_data(Planilha_CC15, coluna) 
+processar_datas(Planilha_CC15, colunas_de_data)
+processar_coluna_chegada(Planilha_CC15,'Data Chegada')
+Planilha_CC15 = Planilha_CC15.dropna(axis=1, how='all')
+#print(Planilha_CC15['Data Chegada'])
 
-for i, linha in enumerate(Planilha_CC15.index):
-    data_chegada = Planilha_CC15.loc[linha, "Data Chegada"]
-    data_entrega = Planilha_CC15.loc[linha, "Data Entrega"]   
-    data_fim_descarregamento =  Planilha_CC15.loc[linha, "Fim Descarreg."]  
-    data_nota = Planilha_CC15.loc[linha, "DATA NOTA FISCAL"]  
-    nota = Planilha_CC15.loc[linha, "NF"]
-    print(f'nota:{nota} data:{data_nota}')
+Planilha_Bahia = pd.read_excel("EntregaT2.xlsx")    
+Planilha_Bahia = Planilha_Bahia[['NF', 'STATUS', 'DT NF', 'CHEGADA', 'FIM DESCARGA', 'ENTREGA']]
+Planilha_Bahia = Planilha_Bahia[(Planilha_Bahia['STATUS'] == 'ATRASADA') | (Planilha_Bahia['STATUS'] == 'NO PRAZO')]
+Planilha_Bahia = Planilha_Bahia.rename(columns={'DT NF': 'DATA NOTA FISCAL','CHEGADA': 'Data Chegada', 'ENTREGA': 'Data Entrega', 'FIM DESCARGA': 'Fim Descarreg.'})
+Planilha_Bahia['NF'] = Planilha_Bahia['NF'].astype(np.int64)#problema do .0
+Planilha_Bahia['STATUS'] = 'Entregue'
+Planilha_Bahia['DATA NOTA FISCAL'] = pd.to_datetime(Planilha_Bahia['DATA NOTA FISCAL'])
+Planilha_Bahia['DATA NOTA FISCAL'] = Planilha_Bahia['DATA NOTA FISCAL'].dt.strftime('%d/%m/%Y')
+colunas_para_formatar = ['Data Chegada', 'Data Entrega', 'Fim Descarreg.']
+formatar_datas(Planilha_Bahia, colunas_para_formatar)
+Planilha_Bahia = Planilha_Bahia.dropna(axis=1, how='all')
+# #print(Planilha_Bahia)
+
+
+BASE_DADOS = pd.read_excel("BASE_DADOS.xlsx")
+BASE_DADOS = BASE_DADOS.dropna(axis=1, how='all')
+#print(BASE_DADOS)
+
+# # # # Juntar as 4 planilhas
+combined_df = pd.concat([BASE_DADOS,Planilha_CC19, Planilha_CC15,Planilha_Bahia], ignore_index=True)
+combined_df = combined_df[combined_df['STATUS'] == 'Entregue']
+combined_df = combined_df.drop_duplicates(subset='NF', keep='first')
+combined_df['BAIXADO'] = combined_df['BAIXADO'].fillna('NAO')
+#print(combined_df)
+
+for i, linha in enumerate(combined_df.index):
+    nf = combined_df.loc[linha, "NF"]
+    data_nota_fiscal = combined_df.loc[linha, "DATA NOTA FISCAL"]
+    data_chegada = combined_df.loc[linha, "Data Chegada"]   
+    data_entrega = combined_df.loc[linha, "Data Entrega"]   
+    data_fim_descarregamento =  combined_df.loc[linha, "Fim Descarreg."]  
+    baixado = str(combined_df.loc[linha, "BAIXADO"])  
+    if baixado == "SIM":
+        continue
+    else:
+        data_chegada_str_sem_hora = remover_hora(data_chegada)
+        try:
+            data_datetime = datetime.strptime(data_nota_fiscal, '%d/%m/%Y')
+        except:
+            pass
+        try:
+            data_datetime2 = datetime.strptime(data_chegada_str_sem_hora, '%d/%m/%Y')
+        except:
+            pass
+        try:
+            if data_datetime and data_datetime2:
+                if data_datetime > data_datetime2:
+                    data_datetime = datetime.strptime(data_nota_fiscal, '%m/%d/%Y')
+                    data_str_formatada = data_datetime.strftime('%d/%m/%Y')  
+                    data_nota_fiscal = data_str_formatada  
+        except:
+            pass
+   
+        print(f'nota:{nf} data nota:{data_nota_fiscal} data chegada:{data_chegada} data entrega:{data_entrega} fim descarregamento:{data_fim_descarregamento}')
