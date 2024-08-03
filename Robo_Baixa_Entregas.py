4b5be3d7186a0fd38e17fdf2cf42e1efc5e90a85
Andrew Lucas <andrewl.12.al85@gmail.com>
2024-08-02 22:49:09 -0300

    atualizado dia 02.08

--- Robo_Baixa_Entregas.py
+++ Robo_Baixa_Entregas.py
@@ -327,80 +327,80 @@ def formatar_datas(df, colunas):
         df[coluna] = df[coluna].dt.strftime('%d/%m/%Y%H:%M')
     
 
-# driver = webdriver.Chrome()
-# driver.get("https://jettatransporte-my.sharepoint.com/:f:/g/personal/jetta_bi_jettatransporte_onmicrosoft_com/EiA6eCcrmHVOi0SVjgVS4eYBTgW6NmdHNlvRSINLlAOW5g?e=qyl9wK")
-# driver.maximize_window()
+driver = webdriver.Chrome()
+driver.get("https://jettatransporte-my.sharepoint.com/:f:/g/personal/jetta_bi_jettatransporte_onmicrosoft_com/EiA6eCcrmHVOi0SVjgVS4eYBTgW6NmdHNlvRSINLlAOW5g?e=qyl9wK")
+driver.maximize_window()
 
-# click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span/button')
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span/button')
                           
-# click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span/button')
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[1]/span/span/button')
 
-# try:
-#     print("Pasta Planilha Bahia...")
-#     corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
-#     action_chains = ActionChains(driver)                                                
-#     action_chains.context_click(corpo_email).perform()
-# except Exception as e:
-#     print("Erro ao clicar no corpo do e-mail:", e)
-# click_selenium(By.XPATH, '/html/body/div[4]/div/div/div/div/div/div/ul/li[4]/button/div/span')
-# pyautogui.sleep(2)
-# driver.back()
-# pyautogui.sleep(2)
+try:
+    print("Pasta Planilha Bahia...")
+    corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
+    action_chains = ActionChains(driver)                                                
+    action_chains.context_click(corpo_email).perform()
+except Exception as e:
+    print("Erro ao clicar no corpo do e-mail:", e)
+click_selenium(By.XPATH, '/html/body/div[4]/div/div/div/div/div/div/ul/li[4]/button/div/span')
+pyautogui.sleep(2)
+driver.back()
+pyautogui.sleep(2)
 
-# click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div[3]/div/div[1]/span/span/button')
-# try:
-#     print("Pasta Planilha CC15...")
-#     corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
-#     action_chains = ActionChains(driver)
-#     action_chains.context_click(corpo_email).perform()
-# except Exception as e:
-#     print("Erro ao clicar no corpo do e-mail:", e)
-# click_selenium(By.XPATH, '/html/body/div[4]/div/div/div/div/div/div/ul/li[4]/button/div/span')
-# pyautogui.sleep(2)
-# driver.back()
-# pyautogui.sleep(2)
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div[3]/div/div[1]/span/span/button')
+try:
+    print("Pasta Planilha CC15...")
+    corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
+    action_chains = ActionChains(driver)
+    action_chains.context_click(corpo_email).perform()
+except Exception as e:
+    print("Erro ao clicar no corpo do e-mail:", e)
+click_selenium(By.XPATH, '/html/body/div[4]/div/div/div/div/div/div/ul/li[4]/button/div/span')
+pyautogui.sleep(2)
+driver.back()
+pyautogui.sleep(2)
 
-# click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[3]/div/div/div[3]/div/div[1]/span/span/button')
-# try:
-#     print("Pasta Planilha CC19...")
-#     corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
-#     action_chains = ActionChains(driver)
-#     action_chains.context_click(corpo_email).perform()
-# except Exception as e:
-#     print("Erro ao clicar no corpo do e-mail:", e)
-# click_selenium(By.XPATH, '/html/body/div[4]/div/div/div/div/div/div/ul/li[4]/button/div/span')
+click_selenium(By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div[3]/div/div/div[3]/div/div[1]/span/span/button')
+try:
+    print("Pasta Planilha CC19...")
+    corpo_email = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="appRoot"]/div/div[2]/div/div/div[2]/div[2]/main/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/div/div[3]/div/div[1]/span/span/button')))
+    action_chains = ActionChains(driver)
+    action_chains.context_click(corpo_email).perform()
+except Exception as e:
+    print("Erro ao clicar no corpo do e-mail:", e)
+click_selenium(By.XPATH, '/html/body/div[4]/div/div/div/div/div/div/ul/li[4]/button/div/span')
 
-# pyautogui.sleep(5)
-# driver.quit()
+pyautogui.sleep(5)
+driver.quit()
 
-# arquivos = [
-#     'EntregaT2.xlsx',
-#     'planilhaderotascc15.xlsx',
-#     'planilhaderotascc19.xlsx'
-# ]
+arquivos = [
+    'EntregaT2.xlsx',
+    'planilhaderotascc15.xlsx',
+    'planilhaderotascc19.xlsx'
+]
 
-# diretorio_origem = r'C:/Users/Usuario/Downloads/'
-# diretorio_destino = r'C:\Users\Usuario\Desktop\Robo-Baixa-Entregas'
-# # diretorio_origem = r'C:/Users/Andrew/Downloads/'
-# # diretorio_destino = r'C:/Users/Andrew/Desktop/Robo-Baixa-Entregas'
+diretorio_origem = r'C:/Users/Usuario/Downloads/'
+diretorio_destino = r'C:\Users\Usuario\Desktop\Robo-Baixa-Entregas'
+# diretorio_origem = r'C:/Users/Andrew/Downloads/'
+# diretorio_destino = r'C:/Users/Andrew/Desktop/Robo-Baixa-Entregas'
 
-# if not os.path.exists(diretorio_destino):
-#     os.makedirs(diretorio_destino)
+if not os.path.exists(diretorio_destino):
+    os.makedirs(diretorio_destino)
 
-# for arquivo in arquivos:
-#     caminho_origem = os.path.join(diretorio_origem, arquivo)
-#     caminho_destino = os.path.join(diretorio_destino, arquivo)
-#     try:
-#         if os.path.exists(caminho_origem):
-#             shutil.move(caminho_origem, caminho_destino)
-#             print(f"Arquivo '{arquivo}' movido com sucesso para '{diretorio_destino}'")
-#         else:
-#             print(f"O arquivo de origem '{caminho_origem}' não existe.")
-#     except Exception as e:
-#         print(f"Erro ao mover o arquivo '{arquivo}': {e}")
+for arquivo in arquivos:
+    caminho_origem = os.path.join(diretorio_origem, arquivo)
+    caminho_destino = os.path.join(diretorio_destino, arquivo)
+    try:
+        if os.path.exists(caminho_origem):
+            shutil.move(caminho_origem, caminho_destino)
+            print(f"Arquivo '{arquivo}' movido com sucesso para '{diretorio_destino}'")
+        else:
+            print(f"O arquivo de origem '{caminho_origem}' não existe.")
+    except Exception as e:
+        print(f"Erro ao mover o arquivo '{arquivo}': {e}")
 
-# driver.quit()
-# pyautogui.sleep(5)
+driver.quit()
+pyautogui.sleep(5)
 
 Planilha_CC19 = pd.read_excel("planilhaderotascc19.xlsx")
 colunas_para_remover = ['Série', 'Cnpj cliente', 'N° Carga', 'Status da baixa','Cliente','Cidade','Ct-e/OST','Peso','Qtde','Vlr Merc.','Entrega Canhoto Físico','NF com problema','Imagem Salva - Com Erro']
