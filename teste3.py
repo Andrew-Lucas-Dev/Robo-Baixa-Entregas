4de0c0d073372f164b0f5d1f16ca3d85c9c40e64
Monsterned <andrewl.12.al85@gmail.com>
2024-08-08 13:34:06 -0300

    atualizacao nao terminada

--- teste3.py
+++ teste3.py
@@ -48,7 +48,6 @@ caminho_sistema = caminho.replace("C", "T", 1)
 
 # confirmacao_preenchido('referencia.png','digitar_data.png','digitar_nota.png')
 
-
 def click_image(image_path, confidence=0.9):
     current_dir = os.path.dirname(__file__)  # Diretório atual do script
     caminho_imagem = caminho + r'\IMAGENS'
@@ -57,13 +56,62 @@ def click_image(image_path, confidence=0.9):
         try:
             position = pyautogui.locateOnScreen(image_path, confidence=confidence)
             if position:
+                center_x = position.left + position.width // 2
+                center_y = position.top + position.height // 2
+                pyautogui.click(center_x, center_y)
                 print("Imagem foi encontrada na tela.")
-                
+                break
         except Exception as e:
             print("Imagem não encontrada na tela. Aguardando...")
+        pyautogui.sleep(1)
+
+def confirmacao_preenchido(image_path, image_path2, image_path3, confidence=0.9):
+    current_dir = os.path.dirname(__file__)  # Diretório atual do script
+    caminho_imagem = caminho + r'\IMAGENS'
+    image_path = os.path.join(current_dir, caminho_imagem, image_path)
+    image_path2 = os.path.join(current_dir, caminho_imagem, image_path2)
+    image_path3 = os.path.join(current_dir, caminho_imagem, image_path3)
+    a, b, c = 0, 0, 0
+    while True:
+        try:
+            position = pyautogui.locateOnScreen(image_path, confidence=confidence)
+            if position:
+                print("Imagem 1 foi encontrada na tela.")
+                a = 0  # Redefine 'a' se a imagem 1 for encontrada
+                click_image('referencia.png')
+                for i in range(10):
+                    pyautogui.press("backspace")
+                pyautogui.write(str(referencia))
+            else:
+                a = 1  # Define 'a' como 1 se a imagem 1 não for encontrada
+        except Exception as e:
+            print("Erro ao procurar imagem 1:", e)
+            a = 1
+        try:
+            position2 = pyautogui.locateOnScreen(image_path2, confidence=confidence)
+            if position2:
+                print("Imagem 2 foi encontrada na tela.")
+                b = 0  # Redefine 'b' se a imagem 2 for encontrada
+            else:
+                b = 1  # Define 'b' como 1 se a imagem 2 não for encontrada
+        except Exception as e:
+            print("Erro ao procurar imagem 2:", e)
+            b = 1
+        try:
+            position3 = pyautogui.locateOnScreen(image_path3, confidence=confidence)
+            if position3:
+                print("Imagem 3 foi encontrada na tela.")
+                c = 0  # Redefine 'c' se a imagem 3 for encontrada
+            else:
+                c = 1  # Define 'c' como 1 se a imagem 3 não for encontrada
+        except Exception as e:
+            print("Erro ao procurar imagem 3:", e)
+            c = 1
+        # Se todas as três imagens não forem encontradas, sai do loop
+        if a == 1 and b == 1 and c == 1:
+            print("Nenhuma das três imagens foi encontrada. Saindo do loop.")
             break
         pyautogui.sleep(1)
 
-click_image('referencia.png')
-click_image('digitar_data.png')
-click_image('digitar_nota.png')
+referencia = 123456789
+confirmacao_preenchido('referencia.png', 'digitar_data.png', 'digitar_nota.png')
